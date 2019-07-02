2837e61cee6c0bebafc7988021ed640e23cd5d22
Franklin Chang <frnklnchng@gmail.com>
2019-07-02 13:47:38 -0700

    Setup users creation and login

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const app = express();
 const db = require('./config/keys').mongoURI;
+
+const users = require("./routes/api/users");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -10,5 +14,12 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Everything is fine!"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use("/api/users", users); // Users route
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
