cafbeeb1bb435e1526e2cb395115807a4c0d726c
Franklin Chang <frnklnchng@gmail.com>
2019-07-02 13:00:00 -0700

    Initial commit

--- src/config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  gameWidth: 760,
-  gameHeight: 400,
-  localStorageName: 'future',
-  webfonts: ['Bangers']
-}
--- src/lang.js
+++
@@ -1,9 +0,0 @@
-import idiom from 'idiom.js'
-
-const lang = idiom({
-  'default': {
-    'welcome': 'future'
-  }
-})
-
-export default lang(window.navigator.language)
